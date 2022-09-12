3207094636509d65400b32f288f9f35d482750f3
Kateryna <dev.kateryna@gmail.com>
2022-09-12 13:25:44 +0100

    Test with same ending letters passed

--- PairExersice.Tests/PairTests.cs
+++ PairExersice.Tests/PairTests.cs
@@ -32,14 +32,18 @@ namespace PairExersice.Tests
         [Test]
         public void ReverseWordsInArray_Should_Return_Correct_Array()
         {
-            //arrange
             string[] input = { "how", "are", "you" };
             string[] expetedOutput = { "woh", "era", "uoy" };
-
-            //act
             string[] actualOutput = WordManager.ReverseWordsInArray(input);
+            actualOutput.Should().BeEquivalentTo(expetedOutput);
+        }
 
-            //assert
+        [Test]
+        public void GetSortedArrayFromString_Should_Return_Correct_Array_With_Same_End_Latters()
+        {
+            string input = "www how wow are you now";
+            string[] expetedOutput = { "are", "you", "www", "wow", "how", "now" };
+            string[] actualOutput = WordManager.GetSortedsArrayFromString(input);
             actualOutput.Should().BeEquivalentTo(expetedOutput);
         }
     }
